97bf3d537611440f1c09eeb129d09793ff6976fd
Jacky2k <fabian@opencode.eu>
2011-03-12 13:02:13 +0100

    Added GPL license notices.

--- RepRapHost.cpp
+++ RepRapHost.cpp
@@ -1,5 +1,19 @@
 /*
- * RepRapHost
+ * This file is part of RepRap Minihost.
+ *
+ * RepRap Minihost is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * RepRap Minihost is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  * The RepRapHost class is a simple g-code transmitter to a RepRap
  * or a similar 3D printer.
  * It requires boost library for interpreting the g-code and the
